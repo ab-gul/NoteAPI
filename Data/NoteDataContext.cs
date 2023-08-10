2ace27b647fb717191384336a3e217df3ed9291a
ab-gul <ab.gulchohra@gmail.com>
2023-08-10 21:30:29 +0200

    added ef core config

---
+++ Data/NoteDataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoteAPI.Models;
+
+namespace NoteAPI.Data
+{
+    public class NoteDataContext:DbContext
+    {
+        public NoteDataContext (DbContextOptions<NoteDataContext> options) : base(options) { }
+     public DbSet<Note> Note { get; set;}
+    }
+}
