db23462d501079d3e16b2eaf4bac6376327567cb
ab-gul <ab.gulchohra@gmail.com>
2023-08-10 21:31:52 +0200

    added interfaces for services

---
+++ Properties/Services/INoteService.cs
@@ -0,0 +1,16 @@
+﻿using NoteAPI.Models;
+using NoteAPI.Repositories;
+
+namespace NoteAPI.Services
+{
+    public interface INoteService
+    {
+      
+        Task<List<Note>> GetAllNotesAync();
+        Task<Note?> GetNoteByIdAsync(Guid id);
+        Task DeleteNoteAsync(Guid id);
+        Task<Note?> AddNoteAsync(Note newNote);
+        Task UpdateNoteAsync(Note newNote);
+
+    }
+}
---
+++ Properties/Services/NoteService.cs
@@ -0,0 +1,44 @@
+﻿using NoteAPI.Data;
+using NoteAPI.Models;
+using NoteAPI.Repositories;
+
+namespace NoteAPI.Services
+{
+    public class NoteService : INoteService
+    {
+        private readonly INoteRepository noteService;
+
+        public NoteService(INoteRepository noteService)
+        { 
+        this.noteService = noteService; 
+        }
+
+        public async Task<Note?> AddNoteAsync(Note newNote)
+        {
+           await noteService.AddNoteAsync(newNote);
+           return await noteService.GetNoteByIdAsync(newNote.Id);
+      
+        }
+
+        public async Task DeleteNoteAsync(Guid id)
+        {
+         await noteService.DeleteNoteAsync(id);
+         
+        }
+
+        public async Task<List<Note>> GetAllNotesAync()
+        {
+            return await noteService.GetAllNotesAync();
+        }
+
+        public async Task<Note?> GetNoteByIdAsync(Guid id)
+        {
+            return await noteService.GetNoteByIdAsync(id);
+        }
+
+        public async Task UpdateNoteAsync(Note newNote)
+        {
+            await noteService.UpdateNoteAsync(newNote);
+        }
+    }
+}
