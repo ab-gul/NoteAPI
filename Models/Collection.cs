f55c0dc7a695679a0581e143b96ada397341becc
ab-gul <ab.gulchohra@gmail.com>
2023-08-11 20:24:31 +0200

    added collection model

---
+++ Models/Collection.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NoteAPI.Models
+{
+    public class Collection
+    {
+        [Key]
+        public Guid ID { get; init; } = Guid.NewGuid();
+
+        [Column("Notes")]
+        public List<Note> Notes { get; set; } = new List<Note>();
+
+        [Column("Title")]
+        public string? Title { get; set; } = string.Empty;
+
+        [Column("Description")]
+        public string? Description { get; set; } = string.Empty;
+
+
+        public Collection(Guid id, List<Note> Notes, string? Title, string? Description) => ( )
+            //{id,  Notes,  Title, Description  } = { }
+           // {
+           // this .ID = id;
+           // this .Notes = Notes;
+           // this .Title = Title;
+           // this .Description = Description;
+                
+
+
+           //}
+    }
+}
