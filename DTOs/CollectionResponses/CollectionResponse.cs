4b4a96ffae8475877215dded4db87d55edacc15a
ab-gul <ab.gulchohra@gmail.com>
2023-08-12 00:34:17 +0200

    added Collection Response DTO

---
+++ DTOs/CollectionResponses/CollectionResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace NoteAPI.DTOs.NoteResponses
+{
+    public class CollectionResponse
+    {
+        public class GetCollectionResponse 
+        {
+            public string? Title { get; set; }
+            public string? Description { get; set; }
+            public Guid Id { get; init; }
+            public DateTime CreatedDate { get; set; }
+        }
+        public class UpdateCollectionResponse
+        {
+            public string? Title { get; set; }
+            public string? Description { get; set; }
+            public Guid Id { get; init; }
+            public DateTime CreatedDate { get; set; }
+
+
+        }
+    }
+}
