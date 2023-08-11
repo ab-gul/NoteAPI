2563eceb649e53ae5fc1937c844a0377dfa38435
ab-gul <ab.gulchohra@gmail.com>
2023-08-12 00:35:28 +0200

    added modifications

--- DTOs/NoteResponses/NoteResponses.cs
+++ DTOs/NoteResponses/NoteResponses.cs
@@ -1,18 +1,18 @@
 ﻿namespace NoteAPI.DTOs.NoteResponses
 {
-    public class NoteResponses
+    public class Response
     {
         public class GetNoteResponse 
         {
-            public string Title { get; set; }
-            public string Description { get; set; }
+            public string? Title { get; set; }
+            public string? Description { get; set; }
             public Guid Id { get; init; }
             public DateTime CreatedDate { get; set; }
         }
         public class UpdateNoteResponse
         {
-            public string Title { get; set; }
-            public string Description { get; set; }
+            public string? Title { get; set; }
+            public string? Description { get; set; }
             public Guid Id { get; init; }
             public DateTime CreatedDate { get; set; }
 
