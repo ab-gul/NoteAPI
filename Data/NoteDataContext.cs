8c8686d44b722db52993a5b806d3b249513568df
ab-gul <ab.gulchohra@gmail.com>
2023-08-11 20:24:55 +0200

    registered collection table

--- Data/NoteDataContext.cs
+++ Data/NoteDataContext.cs
@@ -3,9 +3,12 @@ using NoteAPI.Models;
 
 namespace NoteAPI.Data
 {
-    public class NoteDataContext:DbContext
+    public class NoteDataContext : DbContext
     {
-        public NoteDataContext (DbContextOptions<NoteDataContext> options) : base(options) { }
-     public DbSet<Note> Note { get; set;}
+        public NoteDataContext(DbContextOptions<NoteDataContext> options) : base(options) { }
+
+        public DbSet<Note> Note { get; set; }
+
+        public DbSet<Collection> Collection { get; set; }
     }
 }
