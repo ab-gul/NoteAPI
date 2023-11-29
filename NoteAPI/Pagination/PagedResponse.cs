ad9672f40aa2e225449d3c1ee83a983f13af6eb6
Gulchohra Abdurahman <ab.gulchohra@gmail.com>
2023-11-29 21:32:38 +0100

    pagination added

---
+++ NoteAPI/Pagination/PagedResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace NoteAPI.Pagination
+{
+    public class PagedResponse<T>: Response<T>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public Uri FirstPage { get; set; }
+        public Uri LastPage { get; set; } 
+        public int TotalCount { get; set; }
+
+
+        public PagedResponse(T data, int pageNumber, int pageSize)
+        {
+            this.PageNumber = pageNumber;
+            this.PageSize = pageSize;
+            this.Data = data;
+            this.Message = String.Empty;
+            this.Errors = String.Empty;
+            this.Succeeded = true;
+
+
+            
+        }
+    }
+}
